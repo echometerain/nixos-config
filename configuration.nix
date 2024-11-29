d3e1fadee91cee37c97c09029ca8c90df4450d0a
echometerain <echometer@disroot.org>
2024-11-29 13:50:25 -0500

    346 current  2024-11-29 13:50:17  24.05.20240927.f651414  6.6.52                          *

--- configuration.nix
+++ configuration.nix
@@ -36,6 +36,7 @@
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
   };
+  nix.optimise.automatic = true;
 
   # Custom packages
   nixpkgs = {
