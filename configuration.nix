8185a0fee5eb1686cd36acef649822bb8eed68ae
echometerain <echometer@disroot.org>
2025-03-18 22:07:15 -0400

    477 current  2025-03-18 22:06:54  24.11.20250130.666e1b3  6.6.74                          *

--- configuration.nix
+++ configuration.nix
@@ -151,7 +151,7 @@
     # wireless.userControlled.enable = true;
     networkmanager = {
       enable = true;
-      wifi.backend = "iwd";
+      # wifi.backend = "iwd";
     };
 
     # Open ports in the firewall.
