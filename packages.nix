77b6806554766db3d3c8495da98f0e988180b3c0
echometer <echometer@disroot.org>
2025-06-30 08:20:45 -0400

    34 current  2025-06-30 08:20:38  24.11.20250627.a684c58     6.6.94                          *

--- packages.nix
+++ packages.nix
@@ -131,7 +131,7 @@
       sqlitebrowser
       quartus-prime-lite
       kmymoney
-      inputs.claude-desktop.packages."${pkgs.system}".claude-desktop
+      # inputs.claude-desktop.packages."${pkgs.system}".claude-desktop
 
       # Misc apps
       transmission_4-qt6
