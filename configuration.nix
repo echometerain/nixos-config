2fe89e980f15e5f3c915d75d89bb857640cfa308
honghan.li <william@honghan.li>
2026-06-17 21:03:59 -0400

    159         2026-06-17 21:03:52  26.05.20260611.a037402  6.18.35  Unknown                 []              True

--- configuration.nix
+++ configuration.nix
@@ -217,7 +217,6 @@
 
   # # VST support
   # musnix.enable = true;
-  console.useXkbConfig = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
